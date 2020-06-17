33a19524f5a486427803debbd72c44814c1c4c50
Rob Morain <robert.morain@gmail.com>
2020-06-17 17:02:43 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+Symbolic Semantic Memory project
+"""
+
+def run_experiment():
+    """Run an SSM experiment with the given arguments"""
+    pass
+    
+
+if __name__ == "__main__":
+    print("hello")
+    run_experiment()
