0c08aae90971b94295b8c53e3f5ec4c13e0068ba
Robert Morain <robert.morain@gmail.com>
2020-06-18 12:40:01 -0600

    ssm init added

---
+++ ssm.py
@@ -0,0 +1,9 @@
+class SSM():
+    """
+        This is a Symbolic Semantic Memory module used by a transformer. It is used to extract symbolic information from a knowledge graph
+
+        
+        Input: a tensor of word embeddings representing an input sequence
+        Output: a tensor of embedded edges
+    """
+    def __init__(self):
